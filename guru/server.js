376fe69c2cf527d0bb40e169219d32511562df9e
Monica Mantilla <monicamantilla@MacBook-Pro-2.local>
2019-08-03 11:44:51 -0400

    server

--- guru/server.js
+++ guru/server.js
@@ -1,38 +1,22 @@
 const express = require('express');
-const mongoose = require('mongoose');
-// const axios = require('axios');
-const logger = require('morgan')
-
-
-const PORT = 3000;
-
-
-//Initialize Express
-var app = express();
-
-//Use morgan logger for logging requests
-
-app.use(logger("dev"));
-//Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+const cors = require('cors');
+const mongoose = require('mongoose')
+require ('dotenv').config()
+const app = express()
+const PORT = process.env.PORT || 3000;
+app.use(cors())
 app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
-
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/userLog", { useNewUrlParser: true });
-
-mongoose.set('useCreateIndex', true);
-
-//when deployed to heroku
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/userLog";
-
-// mongoose.connect(MONGODB_URI);
-
-
-  
-  // Start the server
-  app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
-  
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true } 
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully")
+})
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter)
+app.listen(PORT, () => {
+    console.log(`Sever is running on port: ${PORT}`)
+})
