b8e09103bfa80647d72200344c119b8fa8f78122
unknown <naultmichael1@gmail.com>
2019-08-13 20:40:20 -0400

    some components up dated

--- guru/src/App.js
+++ guru/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './App.css';
-import Form from "./components/create.account";
-import Account from "./components/account";
-import HomePage from "./components/home";
-import Signup from './components/signup';
-import Navbar from "./components/navbar/navbar.component"
+// import Form from "./components/create.account";
+// import Account from "./components/account";
+// import HomePage from "./components/home";
+// import Signup from './components/signup';
+// import Navbar from "./components/navbar/navbar.component"
+// import Test from "../src/components/test"
+import FixedExpenses from "../src/components/fixed.expenses"
 
 
 
 function App() {
   return (
     <div className="App">
-
+      <FixedExpenses />
+        {/* <Test /> */}
       {/* < Form/>
       < Account/> */}
-      <HomePage/>
+      {/* <HomePage/> */}
 
 
-      <Signup/>
+      {/* <Signup/>
 
-      < Form/>
+      < Form/> */}
 
     </div>
   );
