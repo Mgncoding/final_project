90e5400eddf97b51a00f09f8c909f6733d21addc
Monica Mantilla <monicamantilla@MacBook-Pro-2.local>
2019-08-15 20:43:02 -0400

    app

--- guru/src/components/fixed.expenses.js
+++ guru/src/components/fixed.expenses.js
@@ -5,181 +5,130 @@ export default class FixedExpenses extends Component {
     constructor(props) {
         super(props)
 
-        // this.onChangeRent = this.onChangeRent.bind(this);
-        // this.onChangePhone = this.onChangePhone.bind(this);
-        // this.onChangeInternet = this.onChangeInternet.bind(this);
-        // this.onChangeinsurance = this.onChangeinsurance.bind(this);
-        // this.onChangecar = this.onChangecar.bind(this);
-        // this.onChangeDisplayTotal = this.onChangeDisplayTotal.bind(this);
-        // this.onSubmit = this.onSubmit.bind(this);
-
         this.state = {
             rent: 0,
             phone: 0,
             internet: 0,
             insurance: 0,
-            car:0,
+            car: 0,
             // displayTotal: 0
         }
     }
-    // componentDidMount(){
-    //     this.setState({
 
-    //     })
-    // }
-    // onChangeRent(e) {
-    //     this.setState({
-    //         rent: e.target.value
-    //     })
-    // Need to make an empty array and push all the states into the the sumfunction and call the 
-    // the function into each input 
-      myArray = [];
-        myArraypush(state.rent, 
-            )
-        mySumFunction(myArray) {
-
+    mySumFunction(myArray) {
+        var total = 0;
+        myArray = [];
+        myArray.push(this.state.rent, this.state.phone, this.state.internet, this.state.insurance, this.state.car
+        )
+        for (let i = 0; i < myArray.length; i++) {
+            if (parseInt(myArray[i].value))
+                total += parseInt(myArray[i].value)
         }
-    
-    handleInputChange = event => {
-        const { name, value } = event.target; 
+    }
 
+    handleInputChange = event => {
+        const { name, value } = event.target;
         this.setState({
             [name]: value
         })
-
     }
-    // onChangePhone(e) {
-    //     this.setState({
-    //         phone: e.target.value
-    //     })
-    // }
-    // onChangeInternet(e) {
-    //     this.setState({
-    //         internet: e.target.value
-    //     })
-    // }
-    // onChangeinsurance(e) {
-    //     this.setState({
-    //         insurance: e.target.value
-    //     })
-    // }
-    // onChangecar(e) {
-    //     this.setState({
-    //         car: e.target.value
-    //     })
-    // }
-    // onChangeDisplayTotal(e) {
-    //     this.setState({
-    //         displayTotal: e.target.value
-    //     })
-    // }
-    // onSubmit(e) {
-    //     e.preventDefault()
 
-    //     const expensesFixed = {
-    //         rent: this.state.rent,
-    //         phone: this.state.phone,
-    //         internet: this.state.internet,
-    //         car: this.state.car,
-    //         insurance: this.state.insurance,
-    //     }
-    //     console.log(expensesFixed)
-    // }
     handleFormSubmit = event => {
         event.preventDefault()
-        
+
         this.setState({
             rent: 0,
             phone: 0,
             internet: 0,
             insurance: 0,
-            car:0,
+            car: 0,
             // displayTotal: 0
 
         })
     }
     render() {
         return (
-        <div className="container">
-            <h2> Your Fixed Expenses</h2>
-            
-          <form>
-            <div className="form-group">
-                <label>Monthly Rent/Mortgage: </label>
-                <br/>
-                <input
-                    value={this.state.rent}
-                    name="rent"
-                    onChange={this.handleInputChange}
-                    type="number"
-                    placeholder="Monthly Rent/Mortgage">
-                        </input>
-            </div>
-            <div className="form-group">
-                <label>Phone Bill: </label>
-                <br/>
-                <input
-                    value={this.state.phone}
-                    name="phone"
-                    onChange={this.handleInputChange}
-                    type="number"
-                    placeholder="Phone Bill">
-                        </input>
-
-            </div>
-            {/* <div className="form-group">
-                <label>Car Insurance: </label>
-                <br/>
-                <input
-                    value={this.state.car}
-                    name="car insurance"
-                    onChange={this.handleInputChange}
-                    type="number"
-                    placeholder="Car Insurance">
-                        </input>
-            </div> */}
-            <div className="form-group">
-                <label>Cable/Internet: </label>
-                <br/>
-                <input
-                    value={this.state.internet}
-                    name="internet"
-                    onChange={this.handleInputChange}
-                    type="number"
-                    placeholder="Cable/Internet">
-                        </input>
-            </div>
-            {/* <div className="form-group">
-                <label>Rent/Mortgage Insurance: </label>
-                <br/>
-                <input
-                    value={this.state.insurance}
-                    name="rent insurance"
-                    onChange={this.handleInputChange}
-                    type="number"
-                    placeholder="Rent/Mortgage Insurance">
-                        </input>
-            </div> */}
-                        
-          </form>
-          <br/><br/>
             <div className="container">
-                <label>Your Total Fixed Expenses</label>
-                <input 
-                    value={this.state.displayTotal}
-                    name="total"
-                    onChange={this.handleInputChange}
-                    
-                    placeholder="Display of Total Inputs">
+                <h2> Your Fixed Expenses</h2>
+
+                <form>
+                    <div className="form-group">
+                        <label>Monthly Rent/Mortgage: </label>
+                        <br />
+                        <input
+                            value={this.state.rent}
+                            name="rent"
+                            onChange={this.handleInputChange}
+                            type="number"
+                            placeholder="Monthly Rent/Mortgage">
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <label>Phone Bill: </label>
+                        <br />
+                        <input
+                            value={this.state.phone}
+                            name="phone"
+                            onChange={this.handleInputChange}
+                            type="number"
+                            placeholder="Phone Bill">
+                        </input>
+
+                    </div>
+                    <div className="form-group">
+                        <label>Car Insurance: </label>
+                        <br />
+                        <input
+                            value={this.state.car}
+                            name="car"
+                            onChange={this.handleInputChange}
+                            type="number"
+                            placeholder="CarInsurance">
+                        </input>
+
+                    </div>
+                    <div className="form-group">
+                        <label>Cable/Internet: </label>
+                        <br />
+                        <input
+                            value={this.state.internet}
+                            name="internet"
+                            onChange={this.handleInputChange}
+                            type="number"
+                            placeholder="Cable/Internet">
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <label>Rent Insurance: </label>
+                        <br />
+                        <input
+                            value={this.state.insurance}
+                            name="insurance"
+                            onChange={this.handleInputChange}
+                            type="number"
+                            placeholder="Rent Insurance">
+                        </input>
+                    </div>
+
+                </form>
+                <br /><br />
+                <div className="container">
+                    <label>Your Total Fixed Expenses</label>
+                    <input
+                        value={this.state.displayTotal}
+                        name="total"
+                        onChange={this.handleInputChange}
+
+                        placeholder="Display of Total Inputs">
                     </input>
-                    <button onClick={this.handleFormSubmit}>Submit</button>
-                    
-                    
-            </div>
-            
+                    <button onClick={this.mySumFunction()}>Submit</button>
+
+
+                </div>
+
 
             </div>
-        
+
         )
-        }
+    }
 }
