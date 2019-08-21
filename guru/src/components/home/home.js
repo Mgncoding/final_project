77ff36f98798fa485f1220fb4132227d557422f4
Monica Mantilla <monicamantilla@macbook-pro-2.home>
2019-08-21 16:46:31 -0400

    homepage

--- guru/src/components/home/home.js
+++ guru/src/components/home/home.js
@@ -29,20 +29,11 @@ export default class HomePage extends Component {
                 <div className="mainBody">
 
                     <div className="mainContent">
-                        
-                        <h1 className="text-center">Finance Guru</h1>
-                        <h2 className="text-center container">
-                            Budget problems??
-                       </h2>
-                        <p className="text-center">
-                            Money seems to just disappear? Now you don't have to worry about it, just consult the guru!. This new app is designed strictly for those wanting to get the most out of their finances and their time. Just follow a couple of steps:
-                            -Create an account
-                            -Input your salary into our salary form
-                            -Input your fixed expenses(expenses that don't fluctuate) into our form
-                            -Input your variable expenses (trivial expenses that people incur on a daily basis) into our form. You'll be able to add and remove certain expenses respectively.
-                            -If you have any major debt (most of all do, don't worry) just add it to the Debt form.
-                            -Go to your profile and you'll be able to see your savings (net worth) in the form of constantly evolving pie charts as well as a report telling you of your current status (green for "ok", red for "not ok").
-                            Simple enough! Now you can bring balance to your financial life with just a couple of clicks!
+                                              
+                        <p className="homebody">
+                            Budget Problems? Money seems to just disappear? Now you don't have to worry! Just consult the <b className="boldguru">Finance Guru! </b> 
+                            Withing a few minutes, you can have a clear understanding of how much money you have left over after your montly expenses.
+                            You are only a few clicks away. Start today!
                         </p>
                         <Button />
                     </div>
